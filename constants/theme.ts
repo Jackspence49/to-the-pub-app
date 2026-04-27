51a715e4057e1e10182dc6e0a35ff9d1d73df685
Jack Spence <jack00spence@gmail.com>
2026-04-26 20:39:26 -0400

    Updated app.json

--- constants/theme.ts
+++ constants/theme.ts
@@ -12,7 +12,7 @@ export const Colors = {
     container: '#FFFFFF',
     border: '#EEEEEE',
     icon: '#757575',
-    iconSelected: '#1976D2',
+    iconSelected: '#npm ',
     activePill: '#E3F2FD',
 
     // Filters
